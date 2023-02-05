e13346831f42b9a9bf0089869ea3dd29c8f417eb
Cristian Paniagua <tenondecrpc@gmail.com>
2023-02-04 21:58:28 -0300

    Initial Commit

---
+++ stepcounter/stepcounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  stepcounterApp.swift
+//  stepcounter
+//
+//  Created by Cristian Paniagua on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct stepcounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
